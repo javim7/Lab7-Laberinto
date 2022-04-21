1da81ffdfbc1066dc47830fdafa5cdbbf34acdae
javim7 <61723252+javim7@users.noreply.github.com>
2022-04-21 16:25:19 -0600

    movimeinto

--- src/components/Laberinto/Laberinto.jsx
+++ src/components/Laberinto/Laberinto.jsx
@@ -9,17 +9,10 @@ const Laberinto = ({ title }) => {
 
     const [laberinto, setLaberinto] = useState([]);
     const [tamano, setTamano] = useState(0);
-    const [jugadorPos, setjugadorPos] = useState();
-    const [jugadorPosX, setjugadorPosX] = useState(0);
-    const [jugadorPosY, setjugadorPosY] = useState(0);
-
-    let movimientoHor = 30;
-    let movimientoVer = 40;
-
-    const style = {
-        left: `${jugadorPosX}`,
-        top: `${jugadorPosY}`
-    }
+    const [jugadorPos, setJugadorPos] = useState([1, 1]);
+    const [tecla, setTecla] = useState('');
+    const [movidas, setMovidas] = useState(0);
+    // const [sePuede, setSePuede] = useState(false);
 
     useEffect(() => {
         setTamano(size[0]);
@@ -33,25 +26,110 @@ const Laberinto = ({ title }) => {
                 // console.log(response2)
                 setLaberinto(response2)
             })
+
     }, [tamano])
 
     useEffect(() => {
 
-        const lab2 = laberinto
+
+        const lab2 = [];
+
+        for (let i = 0; i < laberinto.length; i++) {
+            lab2.push([...laberinto[i]])
+        }
+
         for (let i = 0; i < lab2.length; i++) {
 
             for (let j = 0; j < lab2[i].length; j++) {
-
-                console.log(lab2[i][j]);
+                if (lab2[i][j] == "p") {
+                    // console.log(lab2[i][j]);
+                    // console.log("Y", lab2.indexOf(lab2[i]))
+                    // console.log("X", lab2.indexOf(lab2[j]))
+                }
             }
         }
 
-    }, [])
+    }, [laberinto])
 
     useEffect(() => {
 
-    })
+        document.addEventListener("keydown", (event) => {
+            switch (event.key) {
+                case "ArrowLeft":
+                    // console.log("izquierda")
+                    setTecla("ArrowLeft");
+                    setMovidas(prev => prev + 1)
+                    break;
+                case "ArrowRight":
+                    setTecla("ArrowRight");
+                    setMovidas(prev => prev + 1)
+                    break;
+                case "ArrowDown":
+                    setTecla("ArrowDown");
+                    setMovidas(prev => prev + 1)
+                    break;
+                case "ArrowUp":
+                    setTecla("ArrowUp");
+                    setMovidas(prev => prev + 1)
+                    break;
+            }
+        })
 
+    }, [tamano])
+
+    useEffect(() => {
+
+        if (movidas > 0) {
+
+            let movimiento = jugadorPos;
+            let distancia = 1;
+            // console.log(movidas)
+            // console.log(tecla)
+
+            switch (tecla) {
+                case "ArrowLeft":
+                    // console.log("izquierda")
+                    movimiento = [jugadorPos[0], jugadorPos[1] - distancia]
+                    console.log(movimiento)
+                    break;
+                case "ArrowRight":
+                    movimiento = [jugadorPos[0], jugadorPos[1] + distancia]
+                    break;
+                case "ArrowDown":
+                    movimiento = [jugadorPos[0] + distancia, jugadorPos[1]]
+                    break;
+                case "ArrowUp":
+                    movimiento = [jugadorPos[0] - distancia, jugadorPos[1]]
+                    break;
+            }
+
+            const lab2 = [];
+            let sePuede = false;
+
+            console.log(laberinto[movimiento[0]][movimiento[1]])
+            if (laberinto[movimiento[0]][movimiento[1]] == ' ') {
+
+                sePuede = true;
+            }
+
+
+            // console.log(sePuede)
+            for (let i = 0; i < laberinto.length; i++) {
+                lab2.push([...laberinto[i]])
+            }
+
+            if (sePuede) {
+
+                lab2[movimiento[0]][movimiento[1]] = 'p'
+                lab2[jugadorPos[0]][jugadorPos[1]] = ' '
+
+                setJugadorPos([movimiento[0], movimiento[1]])
+                setLaberinto(lab2)
+            }
+
+        }
+
+    }, [movidas])
 
     return (
         <div className="Laberinto">
@@ -60,7 +138,7 @@ const Laberinto = ({ title }) => {
             <div className="board">
                 {laberinto.map(fila => {
                     return (
-                        <div className="gridFila">
+                        <div className="gridFila" key={Math.random()}>
                             {
                                 fila.map(caracter => {
                                     if (caracter == '-') {
@@ -76,7 +154,7 @@ const Laberinto = ({ title }) => {
                                         return <div key={Math.random()} className="piso"></div>
                                     }
                                     else if (caracter == 'p') {
-                                        return <div style={style} key={Math.random()} onKeyDown={handleKeyDown} tabIndex='0' className="jugador"></div>
+                                        return <div key={Math.random()} className="jugador"></div>
                                     }
                                     else if (caracter == 'g') {
                                         return <div key={Math.random()} className="fin"></div>
